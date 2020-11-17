2c119930102d17ca64426530c34dcbec50822b51
Haley Ehrich <62582412+AKAN-LI@users.noreply.github.com>
2020-11-17 14:45:33 +0800

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,12 @@
+
+public class MAIN {
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("Hello world");
+		Exam1 a=new Exam1();
+		a.func1();
+		System.out.println("99乘法表");
+		a.fuc2();
+	}
+
+}
